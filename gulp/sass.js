bfdcda4465ed2ed250cbbdd8d93a8b9dd9eca3fe
Simon Dragsbæk Petersen <dev@nomis.dk>
2016-03-06 15:21:19 +0100

    [SHAME-COMMIT] add code to prep for future issue fix to use uncss

--- gulp/sass.js
+++ gulp/sass.js
@@ -3,6 +3,13 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
 
+/**
+ *	Prep for uncss (not working due to issue #150)
+ *	ref: https://github.com/giakki/uncss/issues/150
+ *
+ *	var uncss = require('gulp-uncss');
+ */
+
 // Import config
 var config = require('./_config');
 
@@ -12,6 +19,9 @@ module.exports = function(gulp, livereload) {
 		return gulp.src(config.sass)
 			.pipe(sass().on('error', sass.logError))
 			.pipe(autoprefixer())
+			// .pipe(uncss({
+			// 	html: config.html
+			// }))
 			.pipe(gulp.dest('dist'))
 			.pipe(livereload());
 	});
