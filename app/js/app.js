a11ed282b525ab782f7e2fc28597bd9dcecd7aad
Simon Dragsbæk Petersen <dev@nomis.dk>
2016-03-06 15:09:34 +0100

    [TASK] reformat dependencies

--- app/js/app.js
+++ app/js/app.js
@@ -1,28 +1,30 @@
 (function() {
 	// No strict because of babel.js
 
+	var modules = [
+		'ui.router',
+		'app.controller.start'
+	];
+
 	angular
-		.module('app', [
-			'ui.router',
-			'app.controller.start'
-		])
+		.module('app', modules)
 		.config(Config);
 
 	Config.$inject = ['$locationProvider', '$stateProvider', '$urlRouterProvider'];
-	
+
 	function Config($locationProvider, $stateProvider, $urlRouterProvider) {
 		$locationProvider.html5Mode(true);
 
 		$stateProvider
 			.state('start', {
-				url: '/', 
-				templateUrl: 'partials/start.html', 
+				url: '/',
+				templateUrl: 'partials/start.html',
 				controller: 'StartController',
 				controllerAs: 'start'
-			}); 
+			});
 
-		$urlRouterProvider 
+		$urlRouterProvider
 			.otherwise('/');
 	}
 
-})(); 
+})();
--- app/js/controllers/start.js
+++ app/js/controllers/start.js
@@ -1,12 +1,16 @@
 (function() {
 	// No strict because of babel.js
 
+	var modules = [
+		'filter.romanize'
+	];
+
 	angular
-		.module('app.controller.start',[])
+		.module('app.controller.start', modules)
 		.controller('StartController', StartController);
 
 	StartController.$inject = [];
-	
+
 	function StartController($scope) {
 		var start = this;
 
