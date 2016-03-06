e95b8f2e1bf3ee57eb263f068bb897e6fae30600
Simon Dragsbæk Petersen <dev@nomis.dk>
2016-03-06 13:54:38 +0100

    [TASK] remove unused scripts

--- gulp/_config.js
+++ gulp/_config.js
@@ -19,8 +19,6 @@ module.exports = {
 	libs: [
 	    'app/vendor/lodash/dist/lodash.core.js',
 	    'app/vendor/angular/angular.js',
-	    'app/vendor/angular-ui-router/release/angular-ui-router.js',
-	    'app/vendor/angular-animate/angular-animate.js',
-	    'app/vendor/angular-aria/angular-aria.js'
+	    'app/vendor/angular-ui-router/release/angular-ui-router.js'
 	]
 };
