327a0ebb59820f5ae1fb364ea09461b4456942b1
sp90 <dev@nomis.dk>
2016-06-07 15:31:35 +0200

    add sass linting

--- gulp/sass.js
+++ gulp/sass.js
@@ -1,3 +1,7 @@
+// Global modules
+var fs  = require('fs');
+var path = require('path');
+
 // Import modules
 var sass = require('gulp-sass');
 var gulpif = require('gulp-if');
@@ -7,11 +11,13 @@ var replace = require('gulp-replace');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var uncss = require('gulp-uncss');
+var yaml = require('js-yaml');
+var sassLint = require('gulp-sass-lint');
 
 // Import config
 var config = require('./_config');
 
-var env = process.env['NODE_ENV'] || 'dev';
+var env = process.env.NODE_ENV || 'dev';
 var live = env === 'prod' || env === 'staging';
 
 // Sass module
@@ -22,19 +28,27 @@ module.exports = function(gulp, livereload) {
 
 	gulp.task('sass', function() {
 		return gulp.src(config.sass)
-        	.pipe(gulpif(live, sourcemaps.init()))
+			.pipe(gulpif(live, sourcemaps.init()))
 			.pipe(sass().on('error', sass.logError))
 			.pipe(autoprefixer())
 			.pipe(gulpif(live, uncss({
 				html: ['dist/**/*.html'],
-     			ignore: [,
-    				/is-/
-     			]
+				ignore: [
+					/is-/
+				]
 			})))
-        	.pipe(gulpif(live, cssnano(nanoConf)))
-	        .pipe(gulpif(live, sourcemaps.write('./')))
-	        .pipe(gulpif(live, replace(/\/\/# sourceMappingURL=.*$/g, '')))
+			.pipe(gulpif(live, cssnano(nanoConf)))
+			.pipe(gulpif(live, sourcemaps.write('./')))
+			.pipe(gulpif(live, replace(/\/\/# sourceMappingURL=.*$/g, '')))
 			.pipe(gulp.dest('dist'))
 			.pipe(livereload());
+	});
+
+	gulp.task('sass-lint', function () {
+		var sassLintConfig = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '../', '.sass-lint.yml'), 'utf8'));
+
+		return gulp.src(config.sass)
+			.pipe(sassLint(sassLintConfig))
+			.pipe(sassLint.format());
 	});
 };
