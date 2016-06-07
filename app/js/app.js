1c83cd8265cff74448721dd255869243f91e1588
sp90 <dev@nomis.dk>
2016-06-08 00:14:07 +0200

    add thirdparty include support angular

--- app/js/app.js
+++ app/js/app.js
@@ -18,27 +18,34 @@
 		.config(Config);
 
 	// Inject dependencies
-	Config.$inject = ['$locationProvider', '$stateProvider', '$urlRouterProvider'];
+	Config.$inject = ['$locationProvider', '$stateProvider', '$urlRouterProvider', '$sceDelegateProvider'];
 
 	// Config app
-	function Config($locationProvider, $stateProvider, $urlRouterProvider) {
+	function Config($locationProvider, $stateProvider, $urlRouterProvider, $sceDelegateProvider) {
 		$locationProvider.html5Mode(true);
 
 		$stateProvider
 			.state('rome', {
 				url: '/',
-				templateUrl: 'partials/rome.html',
+				templateUrl: '/partials/rome.html',
 				controller: 'RomeController',
 				controllerAs: 'rome'
 			})
 			.state('cv', {
 				url: '/cv',
-				templateUrl: 'partials/cv.html',
+				templateUrl: '/partials/cv.html',
 				controller: 'CvController',
 				controllerAs: 'cv'
 			});
 
 		$urlRouterProvider
 			.otherwise('/');
+
+		$sceDelegateProvider.resourceUrlWhitelist([
+			// Allow same origin resource loads.
+			'self',
+			// Allow loading from our assets domain.  Notice the difference between * and **.
+			'https://s3.eu-central-1.amazonaws.com/**'
+		]);
 	}
 })();
