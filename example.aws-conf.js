4b2c94e6fef9653f192b590af9be35bbb502c2ed
sp90 <dev@nomis.dk>
2016-06-08 00:12:26 +0200

    add example config files

---
+++ example.aws-conf.js
@@ -0,0 +1,8 @@
+// Just rename this file to .aws-conf.js
+// Typical url structure:
+// https://s3.REGION.amazonaws.com/BUCKET_NAME
+
+// REMEMBER NO ENDING SLASH
+module.exports = {
+	'url': 'SOME_URL'
+};
---
+++ example.aws-keys.js
@@ -0,0 +1,12 @@
+// Just rename this file to .aws-keys.js
+// To enable it on deploy
+// Generate keys?
+// http://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSGettingStartedGuide/AWSCredentials.html
+module.exports = {
+	'accessKeyId': 'SOME_KEY',
+	'secretAccessKey': 'SOME_SECRET',
+	'region': 'YOUR_REGION',
+	'params': {
+		'Bucket': 'BUCKET_NAME'
+	}
+};
