42110f007bd888965f66b2b4da212c2bcfa6fdd4
sp90 <dev@nomis.dk>
2016-07-06 13:55:41 +0200

    Add scaffold of controllers

--- scaffolding/templates/controller/controller.test.js
+++ scaffolding/templates/controller/controller.test.js
@@ -1,1 +1,20 @@
 // Some test here
+describe('{{nameUpper}} function', function() {
+
+  describe('{{nameUpper}}', function() {
+    var $scope;
+
+    beforeEach(module('app'));
+
+    beforeEach(inject(function($rootScope, $controller) {
+      $scope = $rootScope.$new();
+      $controller('{{nameUpper}}', {
+      	$scope: $scope
+      });
+    }));
+
+    it('should create something', function() {
+      // Expect something
+    });
+  });
+});
